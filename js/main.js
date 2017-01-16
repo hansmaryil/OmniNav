b498c491100a2d9369f3c7934824e6fdf3b99de3
Hans Maryil <hans.maryil@gmail.com>
2017-01-16 18:14:42 -0500

    fixed scrolling issue

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-$('.navbar-scroll').click(function () {
-    var divID = '#' + this.id;
-    $('html, body').animate({
-        scrollTop: $(divID).offset().top
-    }, 2000);
+$(document).ready(function () {
+	$('.navbar-scroll').on('click', function (event) {
+		event.preventDefault();
+	    var divID = '#' + this.getAttribute('data-target');
+	    var scroll_point = $(divID).offset().top - $("#fixed-navbar").outerHeight(true);
+	    $('html, body').animate({
+	        scrollTop: scroll_point
+	    }, 1000);
+	});
 });
