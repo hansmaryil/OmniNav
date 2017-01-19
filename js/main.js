aa5a8007fdd11c73a9b2fc3c7ac36cc6527cd9d3
Hans Maryil <hans.maryil@gmail.com>
2017-01-19 17:10:16 -0500

    setting up the bootstrap blog theme and adding a blog page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-	$('.navbar-scroll').on('click', function (event) {
-		event.preventDefault();
-	    var divID = '#' + this.getAttribute('data-target');
-	    var scroll_point = $(divID).offset().top - $("#fixed-navbar").outerHeight(true);
-	    $('html, body').animate({
-	        scrollTop: scroll_point
-	    }, 1000);
-	});
+    $('.navbar-scroll').on('click', function (event) {
+        event.preventDefault();
+        var divID = '#' + this.getAttribute('data-target');
+        var scroll_point = $(divID).offset().top - $("#fixed-navbar").outerHeight(true);
+        $('html, body').animate({
+            scrollTop: scroll_point
+        }, 1000);
+    });
 });
