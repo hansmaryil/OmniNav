91cdb6212a811861bd9a121758fcd9a9039882fb
hananmaryil <hananmathew@gmail.com>
2017-03-04 15:19:34 -0500

    fixed responsive scolling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
-    $('.navbar-scroll').on('click', function (event) {
-        event.preventDefault();
-        var divID = '#' + this.getAttribute('data-target');
-        var scroll_point = $(divID).offset().top - $("#fixed-navbar").outerHeight(true);
-        $('html, body').animate({
-            scrollTop: scroll_point
-        }, 1000);
+    function collapseNavbar() {
+        $(".navbar").offset().top > 50 ? $(".navbar-fixed-top").addClass("top-nav-collapse") : $(".navbar-fixed-top").removeClass("top-nav-collapse")
+    }
+    $(window).scroll(collapseNavbar), $(document).ready(collapseNavbar), $(function() {
+        $("a.page-scroll").bind("click", function(e) {
+            var divID = '#' + this.getAttribute('data-target');
+            if(divID != '#page-top') {
+                var scroll_point = $(divID).offset().top - $("#fixed-navbar").outerHeight(true);
+            }
+            else {
+                var scroll_point = $(divID).offset().top 
+            }
+            $("html, body").stop().animate({
+                scrollTop: scroll_point
+            }, 1000), e.preventDefault()
+        })
+    }), $(".navbar-collapse ul li a").click(function() {
+        $(this).closest(".collapse").collapse("toggle")
     });
+
 });
